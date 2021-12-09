cc50d53ec93ce5135acbabe5812b6d8c6d81420b
testgit <luuquangtri95@gmail.com>
2021-12-10 00:27:18 +0700

    change common

--- js/utils/common.js
+++ js/utils/common.js
@@ -1,3 +1,5 @@
+import { DIV_CLASS } from '../constants/todo_constants'
+
 export function updateTitle(element, text) {
   const titleElement = element.querySelector('.todo__heading')
   if (!titleElement) return
@@ -22,4 +24,22 @@ export function isMatch(params, liElement) {
   return (
     isMatchSearch(params.get('search'), liElement) && isMatchStatus(params.get('status'), liElement)
   )
+}
+
+export function updateTodoEdited(formCheck, formId) {
+  const liElement = document.querySelector(`ul#todo__list > li[data-id="${formId}"]`)
+  const divClass = liElement.querySelector('div.todo')
+  const titleElement = liElement.querySelector('p.todo__heading')
+
+  const currStatus = formCheck.checked
+
+  titleElement.textContent = formInput.value
+
+  if (currStatus) {
+    divClass.classList.remove(DIV_CLASS.COMPLETED, DIV_CLASS.PENDING)
+    divClass.classList.add(DIV_CLASS.COMPLETED)
+  } else {
+    divClass.classList.remove(DIV_CLASS.COMPLETED)
+    divClass.classList.add(DIV_CLASS.PENDING)
+  }
 }
--- js/utils/todo_form.js
+++ js/utils/todo_form.js
@@ -1,20 +1,5 @@
-import { DIV_CLASS, STATUS } from '../constants/todo_constants'
-
-function showLoading(form) {
-  const button = form.querySelector('[name="submit"]')
-  if (!button) return
-
-  button.textContent = 'loading...'
-  button.disabled = true
-}
-
-function hideLoading(form) {
-  const button = form.querySelector('[name="submit"]')
-  if (!button) return
-
-  button.textContent = 'Submit'
-  button.disabled = false
-}
+import { updateTodoEdited } from '.'
+import { STATUS } from '../constants/todo_constants'
 
 export function initForm({ element, defaultValue, onSubmit }) {
   const form = document.getElementById(element)
@@ -41,21 +26,7 @@ export function initForm({ element, defaultValue, onSubmit }) {
       defaultValue[index] = formInput.value
       defaultValue[index] = formCheck.value
 
-      const liElement = document.querySelector(`ul#todo__list > li[data-id="${form.dataset.id}"]`)
-      const divClass = liElement.querySelector('div.todo')
-      const titleElement = liElement.querySelector('p.todo__heading')
-
-      const currStatus = formCheck.checked
-
-      titleElement.textContent = formInput.value
-
-      if (currStatus) {
-        divClass.classList.remove(DIV_CLASS.COMPLETED, DIV_CLASS.PENDING)
-        divClass.classList.add(DIV_CLASS.COMPLETED)
-      } else {
-        divClass.classList.remove(DIV_CLASS.COMPLETED)
-        divClass.classList.add(DIV_CLASS.PENDING)
-      }
+      updateTodoEdited(formCheck, form.dataset.id)
 
       delete form.dataset.id
 
